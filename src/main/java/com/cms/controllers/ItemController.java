811409bc0036fc4515a1e345417e183121075aae
Joe <joecarroll.dev@gmail.com>
2022-10-09 20:37:40 +0100

    delete item, add navbar and update views

--- src/main/java/com/cms/controllers/ItemController.java
+++ src/main/java/com/cms/controllers/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.cms.model.Items;
@@ -34,6 +35,12 @@ public class ItemController {
 	public String listAllItems(Model model){
 		model.addAttribute("items", itemService.getAllItems());
 		return "listAllItems";
+	}
+	
+	@GetMapping("/items/{id}")
+	public String deleteItem(@PathVariable Integer id) {
+		itemService.deleteItem(id);
+		return "redirect:/items";
 	}
 	
 }
